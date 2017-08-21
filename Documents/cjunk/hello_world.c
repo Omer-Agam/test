52c6189a0999b3ba8c802f6ef7bb162b0f214f97
Omer Agam <omer.agam.007@gmail.com>
2017-08-21 15:05:31 +0300

    added hello world program

---
+++ Documents/cjunk/hello_world.c
@@ -0,0 +1,15 @@
+//hallo world program of awsome!!!!!
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	if(argc > 1)
+	{
+		puts(argv[1]);
+	}
+	else
+	{
+		printf("hallo world\n");
+	}
+return 0;
+}
